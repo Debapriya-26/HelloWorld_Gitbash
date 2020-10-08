e697437a811a665d4e9e480136159b7061ec21c5
Debapriya-26 <banerjee.debapriya9@gmail.com>
2020-10-08 13:05:59 +0530

    Commiting modified changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World") ;
-		System.out.println("My First Java program");
+		System.out.println("My Second Java program");
 	}
 }
